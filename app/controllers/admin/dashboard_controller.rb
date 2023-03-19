e9aa65e5af5df1e6c63488a6d8e61aa0010ab28a
JunqiDu <50341989+JunqiDu@users.noreply.github.com>
2023-03-19 02:40:40 -0500

    Add files via upload

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret"
+  
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
